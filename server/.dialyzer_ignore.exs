10f119f141971b70f6387c7ac10e9d82b192464c
jonas-martinez <cbobbxta.fr@gmail.com>
2021-09-06 17:09:28 +0200

    Fix dialyxir

---
+++ server/.dialyzer_ignore.exs
@@ -0,0 +1,4 @@
+[
+  {"lib/phoenix/router.ex:2:no_return Function call/2 has no local return."},
+  {"lib/phoenix/router.ex:403:pattern_match_cov The pattern variable _@10 can never match the type, because it is covered by previous clauses."}
+]
