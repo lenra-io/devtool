1d0872f5e988093a82d903ad3bb9b92eb5a8d84f
jonas-martinez <cbobbxta.fr@gmail.com>
2021-09-06 16:39:48 +0200

    Fix test

--- client/test/main_test.dart
+++ client/test/main_test.dart
@@ -2,9 +2,9 @@ import 'package:client/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Main test', (WidgetTester tester) async {
-    await tester.pumpWidget(DevTools());
+  test('Main test', () {
+    DevTools devtools = DevTools();
 
-    expect(find.byType(DevTools), findsOneWidget);
+    expect(devtools is DevTools, true);
   });
 }
