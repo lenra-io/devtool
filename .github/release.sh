397f037b381f9f4afc354621b979e21f28cc68df
Flavien Cadet <flavien.cadet@nocturlab.fr>
2021-12-13 16:20:48 +0100

    ci: Fix docker build

---
+++ .github/release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+version=${nextRelease.version} # Get version tag
+version=${version#v} # remove the first `v` char
+
+tag="--tag ${DOCKER_IMAGE}:$version"
+if [[ "$v" != *"-beta."* ]]; then # If the version is a prerelease then don't publish on the short version code
+  tag="${tag} --tag ${DOCKER_IMAGE}:${version%%.*}" # Remove all after the first dot (dot included)
+fi
+
+# build the docker image
+docker buildx build \
+  --output type=image,push=true \
+  --platform "amd64,arm64,arm" \
+  ${tag} \
+  --build-arg CI=true \
+  --build-arg GH_PERSONNAL_TOKEN="${GITHUB_TOKEN}" \
+  --cache-from type=local,src=~/.docker-cache \
+  --cache-to type=local,dest=~/.docker-cache \
+  .
