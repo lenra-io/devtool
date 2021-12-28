0c25e577ab8fd9e247533e1ec2928f5604ee7648
Shiipou <shiishii@nocturlab.fr>
2021-12-28 17:38:20 +0100

    ci: new docker tag using regex (#34)
    * ci: new docker tag using regex
    
    * ci: clean script
    
    * ci: add missing tag
    
    * ci: transform tag extraction code in a single function get_tag
    
    that return a tag variable with all docker --tag arguments.

--- .github/release.sh
+++ .github/release.sh
@@ -2,14 +2,54 @@
 
 set -xe # Show output on the logs
 
-version="$1" # Get version tag
-version="${version#v}" # remove the first `v` char
+function get_tag {
+  version="$1" # Get version tag
+  DOCKER_IMAGE="$2"
+  
+  regex='v([0-9]+.[0-9]+.[0-9]+)(-([a-z]+).([0-9]+))?'
+  
+  if [[ $version =~ $regex ]]; then
+    v="${BASH_REMATCH[1]}"
+    channel="${BASH_REMATCH[3]}"
+    channel_version="${BASH_REMATCH[4]}"
 
-tag="--tag ${DOCKER_IMAGE}:$version"
-if [[ "$version" != *"-beta."* ]]; then # If the version is a prerelease then don't publish on the short version code
-  tag="${tag} --tag ${DOCKER_IMAGE}:${version%%.*} --tag ${DOCKER_IMAGE}:latest" # Remove all after the first dot (dot included)
-else
-  tag="${tag} --tag ${DOCKER_IMAGE}:${version%.*}" # Add a tag without beta number for generic beta image
+    tag="--tag ${DOCKER_IMAGE}:${version#v}"
+
+    regex='([0-9]+).([0-9]+).([0-9]+)'
+    if [[ $v =~ $regex ]]; then
+      major=${BASH_REMATCH[1]}
+      minor=${BASH_REMATCH[2]}
+      patch=${BASH_REMATCH[3]}
+
+      arr_version=( "${major}" "${major}.${minor}" "${major}.${minor}.${patch}" )
+      if [[ -n "${channel}" ]]; then
+        tag="${tag} --tag ${DOCKER_IMAGE}:${channel}"
+        for i in "${arr_version[@]}"; do
+          tag="${tag} --tag ${DOCKER_IMAGE}:${i}-${channel}"
+        done
+      else
+        tag="--tag ${DOCKER_IMAGE}:latest"
+        for i in "${arr_version[@]}"; do
+          tag="${tag} --tag ${DOCKER_IMAGE}:${i}"
+        done
+      fi
+      return 0
+    else
+      echo "Version '$v' didn't pass Regex '$regex'." 1>&2
+      return 1
+    fi
+  else
+    echo "Version '$version' didn't pass Regex '$regex'." 1>&2
+    return 1
+  fi
+}
+
+VERSION="$1" # Get version tag
+
+get_tag "$VERSION" "${DOCKER_IMAGE}"
+exit_code=$?
+if [[ "$exit_code" != "0" ]]; then
+  exit $exit_code
 fi
 
 # build the docker image
@@ -22,3 +62,4 @@ docker buildx build \
   --cache-from type=local,src=~/.docker-cache \
   --cache-to type=local,dest=~/.docker-cache \
   .
+
