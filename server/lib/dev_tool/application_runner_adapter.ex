97ffe6b4d8b2d775a3728b06d98796baab1ad57d
Emric Pichonnier <emric.pichonnier@gmail.com>
2022-02-18 11:09:40 +0100

    correct error message

--- server/lib/dev_tool/application_runner_adapter.ex
+++ server/lib/dev_tool/application_runner_adapter.ex
@@ -72,7 +72,7 @@ defmodule DevTool.ApplicationRunnerAdapter do
   defp response({:ok, %Finch.Response{status: status_code, body: body}}, _)
        when status_code not in [200, 202] do
     Logger.error("Application error (#{status_code}) #{body}")
-    {:error, "(#{status_code}) #{body}"}
+    {:error, "Application error (#{status_code}) #{body}"}
   end
 
   @impl true
