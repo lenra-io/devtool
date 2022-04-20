29ffc9a7d79a5286eeb36da37b0ec318275e5e64
jonas-martinez <cbobbxta.fr@gmail.com>
2022-04-20 12:30:04 +0200

    More work

--- server/lib/dev_tool/terminal.ex
+++ server/lib/dev_tool/terminal.ex
@@ -9,16 +9,32 @@ defmodule DevTool.Terminal do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
 
+  @impl true
   def init(opts \\ []) do
     send(self(), :listen)
 
     {:ok, opts}
   end
 
+  @impl true
   def handle_info(:listen, state) do
     get_line()
 
     {:noreply, state}
+  end
+
+  @impl true
+  def handle_call(:stop, _from, state) do
+    state
+    |> Keyword.get(:pid)
+    |> :exec.kill(15)
+    |> case do
+      :ok ->
+        {:reply, :ok, Keyword.put(state, :pid, nil)}
+
+      err ->
+        {:reply, err, state}
+    end
   end
 
   defp get_line() do
@@ -26,7 +42,7 @@ defmodule DevTool.Terminal do
       # Reload watchdog
       "R\n" ->
         IO.puts("Reloading watchdog")
-        GenServer.call(DevTool.Watchdog, :stop)
+        DevTool.Watchdog.restart()
 
       _ ->
         nil
