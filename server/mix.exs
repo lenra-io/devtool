da5f25c4e8d265540243dc15785c582f10850585
Emric Pichonnier <emric.pichonnier@lenra.me>
2022-04-20 14:42:05 +0200

    feat: update application runner to data 9 (#91)

--- server/mix.exs
+++ server/mix.exs
@@ -67,7 +67,7 @@ defmodule DevTool.MixProject do
         host: "github.com",
         project: "lenra-io/application-runner.git",
         credentials: "shiipou:#{System.get_env("GH_PERSONNAL_TOKEN")}",
-        tag: "v1.0.0-beta.25"
+        tag: "v1.0.0-data.9"
       )
     ]
   end
