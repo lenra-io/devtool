c4d4617068e8c0c4653f696a7cfb77903075b3d3
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-04-25 10:20:39 +0200

    feat: Data Controller (#95)

--- server/lib/dev_tool/router.ex
+++ server/lib/dev_tool/router.ex
@@ -8,4 +8,11 @@ defmodule DevTool.Router do
   scope "/api", DevTool do
     get("/apps/test/resources/:resource", ResourcesController, :get_app_resource)
   end
+
+  scope "/app", DevTool do
+    get("/datastore/:datastore/data/:id", DataController, :get)
+    post("/datastore/:datastore/data", DataController, :create)
+    delete("/datastore/:datastore/data/:id", DataController, :delete)
+    put("/datastore/:datastore/data/:id", DataController, :update)
+  end
 end
