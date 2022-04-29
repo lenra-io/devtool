1652a1e41319e03ce383ebdfff55a93507e14d7f
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-04-29 11:46:35 +0200

    feat: Handle ofwatchdog reload (#80)

--- server/config/releases.exs
+++ server/config/releases.exs
@@ -6,8 +6,5 @@ config :dev_tools, DevTool.Endpoint,
 
 config :dev_tools,
   of_watchdog: System.fetch_env!("OF_WATCHDOG_BIN"),
-  upstream_url: System.fetch_env!("OF_WATCHDOG_UPSTREAM_URL"),
-  fprocess: System.fetch_env!("OF_WATCHDOG_F_PROCESS"),
   port: System.get_env("OF_WATCHDOG_PORT", "3333"),
-  mode: System.get_env("OF_WATCHDOG_MODE", "http"),
   application_url: "http://localhost:#{System.get_env("OF_WATCHDOG_PORT", "3333")}"
