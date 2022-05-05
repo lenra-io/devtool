1b9ba2c79fa85d89e06145582f718c314190db33
Emric Pichonnier <emric.pichonnier@lenra.me>
2022-05-05 09:17:38 +0200

    fix

--- server/config/releases.exs
+++ server/config/releases.exs
@@ -4,6 +4,12 @@ config :dev_tools, DevTool.Endpoint,
   http: [port: 4000],
   server: true
 
+config :dev_tools, DevTool.Repo,
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PASSWORD"),
+  database: System.get_env("POSTGRES_DB"),
+  hostname: System.get_env("POSTGRES_HOST")
+
 config :dev_tools,
   of_watchdog: System.fetch_env!("OF_WATCHDOG_BIN"),
   port: System.get_env("OF_WATCHDOG_PORT", "8080"),
