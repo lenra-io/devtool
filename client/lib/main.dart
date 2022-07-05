ad8c0d05f64fc7e958fcd159191e0afd00ed7de6
jonas-martinez <cbobbxta.fr@gmail.com>
2022-07-05 15:24:40 +0200

    feat: Update to match lenra_ui_runner

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:client/models/dev_tools_socket_model.dart';
 import 'package:client_app/app.dart';
 import 'package:client_app/models/socket_model.dart';
-import 'package:client_common/models/user_application_model.dart';
-import 'package:client_common/views/stateful_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:lenra_components/lenra_components.dart';
-import 'package:lenra_ui_runner/lenra_application_model.dart';
 import 'package:provider/provider.dart';
 import 'package:device_preview/device_preview.dart';
 
@@ -27,33 +24,22 @@ class DevTools extends StatelessWidget {
 
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider<UserApplicationModel>(create: (context) => UserApplicationModel()),
-        ChangeNotifierProvider<LenraApplicationModel>(
-          create: (context) => LenraApplicationModel('http://localhost:4000', appName, ''),
-        ),
         ChangeNotifierProvider<SocketModel>(
           create: (context) => DevToolsSocketModel(),
         ),
       ],
       builder: (BuildContext context, _) {
-        return StatefulWrapper(
-          onInit: () {
-            context.read<UserApplicationModel>().currentApp = appName;
-          },
-          builder: (context) {
-            return LenraTheme(
-              themeData: themeData,
-              child: MaterialApp(
-                title: 'Lenra - DevTools',
-                theme: ThemeData(
-                  textTheme: TextTheme(bodyText2: themeData.lenraTextThemeData.bodyText),
-                ),
-                locale: DevicePreview.locale(context),
-                builder: DevicePreview.appBuilder,
-                home: App(appName: appName),
-              ),
-            );
-          },
+        return LenraTheme(
+          themeData: themeData,
+          child: MaterialApp(
+            title: 'Lenra - DevTools',
+            theme: ThemeData(
+              textTheme: TextTheme(bodyText2: themeData.lenraTextThemeData.bodyText),
+            ),
+            locale: DevicePreview.locale(context),
+            builder: DevicePreview.appBuilder,
+            home: App(appName: appName),
+          ),
         );
       },
     );
