59ad7d5db3c23d14d058a5e2f079351818d208e4
Thomas DA ROCHA <8969556+taorepoara@users.noreply.github.com>
2022-08-18 18:11:55 +0200

    feat: Define OF watchdog url with env var

--- server/config/releases.exs
+++ server/config/releases.exs
@@ -13,4 +13,4 @@ config :dev_tools, DevTool.Repo,
 config :dev_tools,
   of_watchdog: System.fetch_env!("OF_WATCHDOG_BIN"),
   port: System.get_env("OF_WATCHDOG_PORT", "8080"),
-  application_url: "http://localhost:#{System.get_env("OF_WATCHDOG_PORT", "8080")}"
+  application_url: System.get_env("OF_WATCHDOG_URL", "http://localhost:#{System.get_env("OF_WATCHDOG_PORT", "8080")}"
