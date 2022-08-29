1c893097bc67135960d2073b58ebae146e6cd8d1
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-08-29 14:20:46 +0200

    feat: Update to match lenra_ui_runner (#129)

--- client/lib/homePage.dart
+++ client/lib/homePage.dart
@@ -1,7 +1,6 @@
-import 'package:client_app/app.dart';
-import 'package:client_app/models/socket_model.dart';
 import 'package:flutter/material.dart';
-import 'package:lenra_ui_runner/lenra_application_model.dart';
+import 'package:lenra_ui_runner/app.dart';
+import 'package:lenra_ui_runner/models/socket_model.dart';
 import 'package:provider/provider.dart';
 
 import 'models/dev_tools_socket_model.dart';
@@ -17,15 +16,18 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      ChangeNotifierProvider<LenraApplicationModel>(
-        create: (context) => LenraApplicationModel('http://localhost:4000', appName, ''),
-      ),
-      ChangeNotifierProvider<SocketModel>(
-        create: (context) {
-          return DevToolsSocketModel(getUserId());
-        },
-      ),
-    ], builder: (BuildContext context, _) => App(appName: appName));
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider<SocketModel>(
+            create: (context) {
+              return DevToolsSocketModel(getUserId());
+            },
+          ),
+        ],
+        builder: (BuildContext context, _) => App(
+              appName: appName,
+              httpEndpoint: "http://localhost:4000",
+              accessToken: "",
+            ));
   }
 }
