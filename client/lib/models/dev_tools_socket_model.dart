5e9dd20938d560319840f563c3591acaa19930e9
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-08-29 14:20:46 +0200

    feat: Update to match lenra_ui_runner (#129)

--- client/lib/models/dev_tools_socket_model.dart
+++ client/lib/models/dev_tools_socket_model.dart
@@ -1,8 +1,8 @@
-import 'package:client_app/models/socket_model.dart';
-import 'package:client_app/socket/lenra_channel.dart';
 import 'package:client_common/utils/connexion_utils_stub.dart'
     if (dart.library.io) 'package:client_common/utils/connexion_utils_io.dart'
     if (dart.library.js) 'package:client_common/utils/connexion_utils_web.dart';
+import 'package:lenra_ui_runner/models/socket_model.dart';
+import 'package:lenra_ui_runner/socket/lenra_channel.dart';
 import 'package:phoenix_wings/phoenix_wings.dart';
 
 class DevToolsSocketModel extends SocketModel {
