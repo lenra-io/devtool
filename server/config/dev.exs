e862c96db38e79e7a9d6554b1c47f1522e831c08
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2023-02-23 11:52:23 +0100

    feat: Cron implementation (#161)

--- server/config/dev.exs
+++ server/config/dev.exs
@@ -9,6 +9,14 @@ config :dev_tools, DevTool.Repo,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
+config :application_runner, ApplicationRunner.Repo,
+  username: System.get_env("POSTGRES_USER", "postgres"),
+  password: System.get_env("POSTGRES_PASSWORD", "postgres"),
+  database: System.get_env("POSTGRES_DB", "devtools_dev"),
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
+
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
---
+++ server/rel/commands/migrate.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+release_ctl eval --mfa "DevTool.MigrationHelper.migrate/0"
