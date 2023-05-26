1602dc2576c86cc30ce1a5fa3258b884b770f9fe
jonas-martinez <cbobbxta.fr@gmail.com>
2023-05-26 17:04:30 +0200

    Remove useless edit

--- server/lib/dev_tool/controllers/index_controller.ex
+++ server/lib/dev_tool/controllers/index_controller.ex
@@ -1,8 +1,6 @@
 defmodule DevTool.IndexController do
   use DevTool, :controller
 
-  require Logger
-
   def index(conn, _params) do
     conn
     |> Plug.Conn.send_file(200, Path.join(:code.priv_dir(:dev_tools), "static/index.html"))
