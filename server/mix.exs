292f99838b886f451a4df495d047fa6bdd3be7bb
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-09-15 17:18:35 +0200

    feat!: Use the new API project with breaking changes

--- server/mix.exs
+++ server/mix.exs
@@ -66,7 +66,7 @@ defmodule DevTool.MixProject do
       {:cors_plug, "~> 3.0"},
       {:application_runner,
        git: "https://github.com/lenra-io/server.git",
-       ref: "v1.0.0-beta.161",
+       ref: "replace-action-by-listener",
        subdir: "libs/application_runner",
        submodules: true},
       {:distillery, "~> 2.1"}
