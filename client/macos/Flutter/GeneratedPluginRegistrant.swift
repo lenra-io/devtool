2dcd7067b15cc005e80f6450947038acf573a92c
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-09-19 14:40:11 +0200

    feat!: Use the new API project with breaking changes (#200)
    * feat!: Use the new API project with breaking changes
    
    * build(deps): Define server version

--- client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,22 @@
 import FlutterMacOS
 import Foundation
 
+import catcher
+import device_info_plus
 import flutter_secure_storage_macos
+import flutter_web_auth_2
+import package_info_plus
 import shared_preferences_foundation
 import url_launcher_macos
+import window_to_front
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CatcherPlugin.register(with: registry.registrar(forPlugin: "CatcherPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
 }
