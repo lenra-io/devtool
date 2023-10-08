0ec21b4945810633f074cf0fad1b86bea8cdff53
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-10-08 09:21:43 +0200

    fix: @lenra:navTo (#206)
    * fix: navTo
    
    * feat: Prepare for release

--- server/mix.exs
+++ server/mix.exs
@@ -66,7 +66,7 @@ defmodule DevTool.MixProject do
       {:cors_plug, "~> 3.0"},
       {:application_runner,
        git: "https://github.com/lenra-io/server.git",
-       ref: "v1.0.0-beta.172",
+       ref: "v1.0.0",
        subdir: "libs/application_runner",
        submodules: true},
       {:distillery, "~> 2.1"}
